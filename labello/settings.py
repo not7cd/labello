dc5587fa60881722d8e59e36ab337d80f6f5cecc
Marcin Jasiukowicz <contact@yasiu.pl>
2026-01-12 21:36:04 +0100

    update configuration instructions; de-hardcode printer and deployment URI/URLs

--- labello/settings.py
+++ labello/settings.py
@@ -1,22 +1,21 @@
 import os
 
-APP_VERSION = "0.1.0"
-
+APP_VERSION = "0.2.0"
 APP_TITLE = "🏷️ labello"
 APP_NAME = "Labello"
+APP_WIKI_URL = "//forum.hsp.sh/t/-/114"
+APP_REPO_URL = "//github.com/not7cd/labello"
 
-APP_BASE_URL = "banana.at.hsp.net.pl:8000"
+APP_HOME_URL = os.environ.get("LABELLO_HOME_URL", "//hsp.sh")
+APP_BASE_URL = os.environ.get("LABELLO_BASE_URL", "localhost:8000")
 
-APP_HOME_URL = "//hsp.sh"
-APP_WIKI_URL = "//wiki.hsp.sh/labello"
-APP_REPO_URL = "//github.com/hspsh/labello"
+APP_IMAGES_PATH = os.environ.get("LABELLO_IMAGES_PATH", "./images/")
 
-APP_IMAGES_PATH = "./images/"
-
-printer_name = "Zebra_LP2824"
+printer_name = os.environ.get("LABELLO_PRINTER_NAME", "Zebra_LP2824")
+printer_host = os.environ.get("LABELLO_PRINTER_HOST", "localhost:631")
 
 labels = {"protected": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
 
-SECRET_KEY = os.environ["APP_SECRET_KEY"]
+SECRET_KEY = os.environ.get("LABELLO_SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY set for Flask application")
