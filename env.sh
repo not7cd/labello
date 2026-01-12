9dfdfd857837aaadc0cfac9c9a8bc7844d4ef088
Marcin Jasiukowicz <contact@yasiu.pl>
2026-01-12 21:36:04 +0100

    update configuration instructions; de-hardcode printer and deployment URI/URLs

--- env.sh
+++ env.sh
@@ -1,3 +1,7 @@
-export APP_SECRET_KEY=$(openssl rand -hex 16)
+export LABELLO_SECRET_KEY=$(openssl rand -hex 16)
 
-env | grep APP_ > .env
+export LABELLO_BASE_URL="localhost:8000"
+export LABELLO_PRINTER_NAME="Zebra_LP2824"
+export LABELLO_PRINTER_HOST="localhost:631"
+
+env | grep LABELLO_ > .env
--- labello/printer.py
+++ labello/printer.py
@@ -1,14 +1,13 @@
 import subprocess
 import tempfile
 import logging
+from labello import settings
 
 logger = logging.getLogger(__name__)
 
-HOST = "192.168.88.119:631"
-
 
 def get_status(printer=""):
-    p = subprocess.Popen(args=["lpstat", "-p", printer], stdout=subprocess.PIPE)
+    p = subprocess.Popen(args=["lpstat", "-h", settings.printer_host, "-p", printer], stdout=subprocess.PIPE)
     for line in iter(p.stdout.readline, b""):
         if printer in line.decode():
             return line.decode()
@@ -19,7 +18,7 @@ def send_raw(data, printer):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".epl") as fp:
         fp.write(data.encode("ISO-8859-1"))  # magic encode
         fp.write("\n\n".encode())
-        command = "lp -h {} -d {} -o raw {}".format(HOST, printer, fp.name)
+        command = "lp -h {} -d {} -o raw {}".format(settings.printer_host, printer, fp.name)
     logger.debug(command)
     res = subprocess.call(command, shell=True)
     return res
